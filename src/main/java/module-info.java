685cf1d83050d56b37b9a06fd880aacd9248f9b8
Emerson Pinter <e@pinter.dev>
2022-04-18 11:46:30 -0300

    Use jpms for tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
+@SuppressWarnings("module")
 module br.com.pinter.tqdatabase {
     exports br.com.pinter.tqdatabase;
     exports br.com.pinter.tqdatabase.models;
+    exports br.com.pinter.tqdatabase.cache to br.com.pinter.tqdatabasetest;
+    exports br.com.pinter.tqdatabase.util to br.com.pinter.tqdatabasetest;
     requires static org.apache.commons.lang3;
 }
---
+++ src/test/java/module-info.java
@@ -0,0 +1,5 @@
+open module br.com.pinter.tqdatabasetest {
+    requires br.com.pinter.tqdatabase;
+    requires org.apache.commons.lang3;
+    requires org.junit.jupiter.api;
+}
