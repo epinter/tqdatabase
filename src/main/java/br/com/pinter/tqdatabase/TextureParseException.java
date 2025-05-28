15b5cc680878453d8e7d5360e05fd2bbb196f692
Emerson Pinter <e@pinter.dev>
2025-05-28 12:29:17 -0300

    Improve TEX/DDS conversion
    Implement handling of reverse mipmap order in conversion to DDS

---
+++ src/main/java/br/com/pinter/tqdatabase/TextureParseException.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2025 Emerson Pinter - All Rights Reserved
+ */
+
+package br.com.pinter.tqdatabase;
+
+public class TextureParseException extends RuntimeException {
+    public TextureParseException(String message) {
+        super(message);
+    }
+}
